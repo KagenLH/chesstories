0d6d9d34631eedd580b9d4bd1eb6db26d221e779
Kagen Hearn <kl.hearn40@gmail.com>
2021-08-30 18:38:01 -0400

    Built skeleton for games-frontend to test receiving information from backend

---
+++ react-app/src/components/Game/Game.js
@@ -0,0 +1,18 @@
+import { useEffect } from 'react';
+import { useDispatch } from 'react-redux';
+
+import './Game.css';
+
+const Game = () => {
+    useEffect(() => {
+
+    });
+
+    return (
+        <div className="game-container">
+            This is the game page.
+        </div>
+    )
+};
+
+export default Game;
